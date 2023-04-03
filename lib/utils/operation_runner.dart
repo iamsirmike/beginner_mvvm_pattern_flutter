161ebf777a570b10ff37d63c62d678dc4462aeca
iamsirmike <asamoahmichael77@gmail.com>
2023-04-03 11:39:53 +0000

    uncomment overlays

--- lib/utils/operation_runner.dart
+++ lib/utils/operation_runner.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 typedef Operation<T> = Future<T> Function();
 
 abstract class OperationRunnerState<T extends StatefulWidget> extends State<T> {
   @protected
+  // ignore: avoid_shadowing_type_parameters
   Future<T?> runOperation<T>(
     String name,
     Operation operation, {
@@ -14,15 +16,15 @@ abstract class OperationRunnerState<T extends StatefulWidget> extends State<T> {
     contextE ??= context;
 
     try {
-      //if (showLoader) context.loaderOverlay.show();
+      if (showLoader) context.loaderOverlay.show();
 
       final result = await operation();
 
-      // if (showLoader) context.loaderOverlay.hide();
+      if (showLoader) context.loaderOverlay.hide();
 
       return result as T;
     } on DioError {
-      // if (showLoader) context.loaderOverlay.hide();
+      if (showLoader) context.loaderOverlay.hide();
 
       // await showSnackMessage(context, e.message, isError: true);
     }
